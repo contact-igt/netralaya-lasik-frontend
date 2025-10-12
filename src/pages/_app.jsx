b40d41b7802ef89054a2ef846baf340329439e3e
Veeravel-S <veeravel.igt@gmail.com>
2025-10-12 20:19:46 +0530

    routing gtm issue fixed

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,9 +4,11 @@ import Header from "@/common/Header";
 import { Popup } from "@/common/Popup";
 import useUTMSource from "@/hooks/useUTMSource";
 import "@/styles/globals.css";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   const [open, setOpen] = useState(false);
   const handleTogglecontactForm = (value) => {
     if (typeof value === "boolean") {
@@ -16,6 +18,28 @@ export default function App({ Component, pageProps }) {
     }
   };
   useUTMSource();
+
+  useEffect(() => {
+    const pushPageview = (url) => {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: "pageview",
+        page: url,
+      });
+      console.log("GTM pageview event pushed:", url);
+    };
+
+    pushPageview(router.asPath);
+
+    const handleRouteChange = (url) => {
+      pushPageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events, router.asPath]);
   return (
     <>
 
