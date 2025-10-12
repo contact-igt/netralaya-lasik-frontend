e6d6cbed0b015e9e076c4d1c777f2de318dd3996
iyyappanmanivelan <iyyappanmani03@gmail.com>
2025-10-12 18:05:56 +0530

    added microsoft clarity

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -25,6 +25,16 @@ export default function Document() {
                      })(window,document,'script','dataLayer','GTM-W7LWHJBR');`,
           }}
         />
+
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "tp0kifn44v");`,
+          }}
+        />
       </Head>
 
       <body>
