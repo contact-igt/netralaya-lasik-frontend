84954ee35b95fd1c5fc4a9166db9cde34ef280e5
Veeravel-S <veeravel.igt@gmail.com>
2025-11-25 16:38:24 +0530

    see more button added

--- src/common/TestimonialCard/index.jsx
+++ src/common/TestimonialCard/index.jsx
@@ -26,6 +26,12 @@ const TestimonialCard = ({
             </div>
             <div className={styles.textContainer}>
                 <p>{testimonial}</p>
+                                          <button
+                    className={styles.seeMoreBtn}
+                    onClick={openModal}
+                >
+                    See More
+                </button>
             </div>
         </div>
     );
